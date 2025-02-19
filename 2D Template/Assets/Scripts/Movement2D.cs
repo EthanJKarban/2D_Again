a50b5b5905495bff7709d84641184c06dd0afe09
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-02-19 11:31:53 -0800

    Hehehe

--- 2D Template/Assets/Scripts/Movement2D.cs
+++ 2D Template/Assets/Scripts/Movement2D.cs
@@ -13,7 +13,7 @@ public class Movement2D : MonoBehaviour
     [SerializeField] private float _maxMoveSpeed = 12f;
     [SerializeField] private float _linearDrag = 10f;
     private float _horizontalDirection;
-    private bool _changingdirection =>  (_rb.velocity.x >  0f && _horizontalDirection < 0f) || (_rb.velocity.x < 0f && _horizontalDirection > 0f);
+    private bool _changingdirection =>  (_rb.linearVelocity.x >  0f && _horizontalDirection < 0f) || (_rb.linearVelocity.x < 0f && _horizontalDirection > 0f);
 
     [Header("Jump Variables")]
     [SerializeField] private float _jumpForce = 12f;
@@ -47,23 +47,23 @@ public class Movement2D : MonoBehaviour
     {
         _rb.AddForce(new Vector2(_horizontalDirection, 0f) * _movementAcceleration);
 
-        if (Mathf.Abs(_rb.velocity.x) > _maxMoveSpeed)
-            _rb.velocity = new Vector2(Mathf.Sign(_rb.velocity.x) * _maxMoveSpeed, _rb.velocity.y);
+        if (Mathf.Abs(_rb.linearVelocity.x) > _maxMoveSpeed)
+            _rb.linearVelocity = new Vector2(Mathf.Sign(_rb.linearVelocity.x) * _maxMoveSpeed, _rb.linearVelocity.y);
     }
     private void ApplyingLinearDrag()
     {
         if (Mathf.Abs(_horizontalDirection) < 0.4f || _changingdirection)
         {
-            _rb.drag = _linearDrag;
+            _rb.linearDamping = _linearDrag;
         }
         else
         {
-            _rb.drag = 0f;
+            _rb.linearDamping = 0f;
         }
     }
     private void Jump()
     {
-        _rb.velocity = new Vector2(_rb.velocity.x, 0f);
+        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0f);
         _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
