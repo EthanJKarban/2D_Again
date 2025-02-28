004568fc8cfd4e42c718c4aab6ff8c3c9d593b5b
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-02-28 12:32:42 -0800

    UEVE

---
+++ 2D Template/Assets/Dererks Lesson stuff/Trigger Event.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+
+public class TriggerEvent : MonoBehaviour
+{
+    //UnityEvenets allow you to create generic scripts instead of repeating yourself across multiple scripts.
+    public UnityEvent onTriggerEnter;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Runs each of the functions assigned in the inspector.
+        onTriggerEnter.Invoke();
+    }
+
+
+    // For a function to be compatible with UntiyEvenets, it must be public , a void, and take a simple variable type.
+    // By simple, I mean string, int, bool, or float. It can't be a Vector or Matrix, or anything complex
+    public void Swticheroo(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+
+}
